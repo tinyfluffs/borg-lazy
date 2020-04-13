7d6cffeae5219c7cc3a9d44f91de4741c3279fd9
Ella Fox <ella@dragonhou.se>
2020-04-13 16:36:09 +0200

    Fix envfile usage

--- restore.sh
+++ restore.sh
@@ -4,20 +4,18 @@ set -ex
 TEMPDIR=$(mktemp -d)
 BACKUP="$1"
 
-echo "$BACKUP"
-
 command -v borg >/dev/null 2>&1 || { echo >&2 "Borg is not installed. Get it from your package manager (usually 'borgbackup' or 'borg').  Aborting."; exit 1; }
 
 command -v sshfs >/dev/null 2>&1 || { echo >&2 "SSH FS is not installed. Get it from your package manager (usually 'sshfs' or 'sshfs-fuse').  Aborting."; exit 2; }
 
-if [ ! -f "env" ]; then
+if [ ! -f "${ENVFILE}" ]; then
 	echo -e "
 	\e[41mEnvironment file not found!
 	Copy the example and edit: \`cp ./env.example ./env\`\e[39m"
 	exit 3;
 fi
 
-source ./env
+source ${ENVFILE}
 
 sshfs -p ${SSH_PORT} ${SSH}:${REMOTE_DIR} ${TEMPDIR}
 cd ${TEMPDIR}
